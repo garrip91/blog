075898626327fe6765b7f58ad9c603fd5bd8d64e
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-18 13:48:23 +0300

    Summary

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 # Create your views here:
 def posts_list(request):
-    return HttpResponse('<h1>Hello, World from blog.views.py!</h1>')
+    # return HttpResponse('<h1>Hello, World from blog.views.py!</h1>')
+    n = ['Oleg', 'Masha', 'Olja', 'Ksu']
+    # return render(request, 'blog/base_blog.html', context={'names': n})
+    return render(request, 'blog/index.html', context={'names': n})
