b3568fa6a4469eee2a5afc68b5e37fd0c00950dd
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-19 15:52:51 +0300

    Summary

--- src/blog/models.py
+++ src/blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name="Заголовок")
     slug = models.SlugField(max_length=150, unique=True, verbose_name="Слаг")
     body = models.TextField(blank=True, db_index=True, verbose_name="Содержание")
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True, verbose_name="Дата")
     
     def get_absolute_url(self):
@@ -15,3 +16,14 @@ class Post(models.Model):
     
     def __str__(self):
         return F'{self.title}'
+        
+        
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="Слаг")
+    
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+    
+    def __str__(self):
+        return F'{self.title}' 
