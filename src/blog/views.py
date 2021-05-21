e2e74f108582f828319429277e68684c48203add
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-05-21 14:19:57 +0300

    Summary

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,11 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Tag
+
+from django.views.generic import View
+
+from .utils import ObjectDetailMixin
 
 
 # Create your views here:
@@ -11,9 +15,12 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
     
     
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    # def get(self, request, slug):
+        # post = get_object_or_404(Post, slug__iexact=slug)
+        # return render(request, 'blog/post_detail.html', context={'post': post})
+    model = Post
+    template = 'blog/post_detail.html'
     
     
 def tags_list(request):
@@ -21,6 +28,9 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
     
     
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    # def get(self, request, slug):
+        # tag = get_object_or_404(Tag, slug__iexact=slug)
+        # return render(request, 'blog/tag_detail.html', context={'tag': tag})
+    model = Tag
+    template = 'blog/tag_detail.html'
